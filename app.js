6f5a647b952db7de4233dc68eb69c80b7be89c7b
magvas <info@magvas.de>
2017-12-23 00:01:18 +0100

    - Changed css lib - added date of birth

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var viewModel = () => {
     this.firstname = ko.observable('');
     this.lastname = ko.observable('');
+    this.dateofbirth = ko.observable('');
     this.fullname = ko.computed(() => {
         return this.firstname() + " " + this.lastname();
     });
